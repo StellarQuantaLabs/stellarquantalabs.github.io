addf069e02c26d35ae3fe6a8c5186b0c51d3be9d
Stellar Quanta Labs <contact@stellarquantalabs.org>
2025-08-01 17:53:58 -0400

    Delete sdr-timelines/dashboard.js

--- sdr-timelines/dashboard.js
+++
@@ -1,6 +0,0 @@
-// JS for dashboard
-document.addEventListener("DOMContentLoaded", () => {
-    let chartDiv = document.getElementById("chart");
-    Plotly.newPlot(chartDiv, [], {title: "Cumulative SDR Timeline (UTC)"});
-    document.getElementById("timeline-grid").innerHTML = "<p>No timelines uploaded yet.</p>";
-});
